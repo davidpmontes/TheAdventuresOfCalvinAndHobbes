74f50c4509222da0f3569624b9fdd97fb1037b45
David Montes <davidpmontes86@gmail.com>
2020-10-11 16:41:39 -0700

    lifebar

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    public static CanvasController Instance { get; private set; }
+
+    [SerializeField] private Sprite[] lifebarSprites = default;
+
+    private Image lifebarImage;
+
+    private void Awake()
+    {
+        lifebarImage = Utils.FindChildByNameRecursively(transform, "CalvinLifebar_Image").GetComponent<Image>();
+        SetLifebar(.5f);
+    }
+
+    public void SetLifebar(float lifePercent)
+    {
+        lifebarImage.sprite = lifebarSprites[Mathf.FloorToInt(lifebarSprites.Length * lifePercent)];
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class Utils
+{
+    public static GameObject FindChildByNameRecursively(Transform parentTransform, string name)
+    {
+        Transform[] children = parentTransform.GetComponentsInChildren<Transform>(true);
+        foreach (var child in children)
+        {
+            if (child.name == name)
+            {
+                return child.gameObject;
+            }
+        }
+        return null;
+    }
+}
